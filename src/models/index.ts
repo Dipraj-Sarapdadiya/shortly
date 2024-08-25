c7f34187827998ee609f771cba90443f6c3ee437
Dipraj S <diprajsarapdadiya@gmail.com>
2024-08-25 17:19:25 +0530

    mongo connection enhanced

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,41 @@
-"use server";
+import mongoose, { Mongoose } from "mongoose";
 
-import mongoose from "mongoose";
+const MONGO_URL: string | undefined = process.env.MONGO_URL;
 
-export const initMongo = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL!);
-  } catch (error) {
-    console.error("Failed to init Mongo: ", error);
+if (!MONGO_URL) {
+  throw new Error("Please define the MONGO_URL environment variable inside .env.local");
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function initMongo(): Promise<Mongoose> {
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URL as string, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
